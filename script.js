9e709b05551dde0da0caf5a5b4f35045b7882c2f
12347484 <134672383+12347484@users.noreply.github.com>
2024-04-17 19:49:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector(".btn.btn-outline-warning");
+
+btn.addEventListener("click", function (e) {
+  location.href = "https://rzp.io/i/sSTUBafc5";
+});
+
+ 
+let btn1 = document.querySelector(".btn.btn-outline-warning.one");
+
+btn1.addEventListener("click", function (e) {
+  location.href = "https://rzp.io/l/HZ2MBgs7n";
+});
+
+let btn2 = document.querySelector(".btn.btn-outline-warning.two");
+
+btn2.addEventListener("click", function (e) {
+  location.href = "https://rzp.io/l/HZ2MBgs7n";
+});
+
+let btn3 = document.querySelector(".btn.btn-outline-warning.three");
+
+btn3.addEventListener("click", function (e) {
+  location.href = "https://rzp.io/l/HZ2MBgs7n";
+}
